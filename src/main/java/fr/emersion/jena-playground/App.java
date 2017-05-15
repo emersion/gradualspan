e998380936a26404fdc4ba5f378535d55c0c5db4
emersion <contact@emersion.fr>
2017-05-15 14:23:43 +0200

    First commit.

---
+++ src/main/java/fr/emersion/jena-playground/App.java
@@ -0,0 +1,13 @@
+package fr.emersion.jena-playground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
