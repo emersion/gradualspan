1e479aacfbb59b62e10cd3f9060011e61286e439
emersion <contact@emersion.fr>
2017-05-15 15:14:50 +0200

    Add basic RDF example

--- src/main/java/fr/emersion/jenaplayground/App.java
+++ src/main/java/fr/emersion/jenaplayground/App.java
@@ -1,13 +1,21 @@
 package fr.emersion.jenaplayground;
 
+import java.io.InputStream;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Model model = ModelFactory.createDefaultModel();
+        InputStream in = FileManager.get().open("vc-db-1.rdf");
+        model.read(in, null);
+        model.write(System.out);
     }
 }
